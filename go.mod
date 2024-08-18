7213740bd3197f242d7545b9005b666d4cdebdf6
coding-ia <137450628+coding-ia@users.noreply.github.com>
2024-08-18 02:36:24 -0700

    Initial

---
+++ go.mod
@@ -0,0 +1,28 @@
+module renovate-webhook-controller
+
+go 1.23.0
+
+require (
+	github.com/aws/aws-lambda-go v1.47.0
+	github.com/coding-ia/renovate-controller v1.0.2
+	github.com/google/go-github/v63 v63.0.0
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2/config v1.27.28 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.28 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.12 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.16 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.16 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ec2 v1.175.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ecs v1.45.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.18 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.22.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.26.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.30.4 // indirect
+	github.com/aws/smithy-go v1.20.4 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/coding-ia/renovate-controller/service"
+	"github.com/google/go-github/v63/github"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	applicationID := os.Getenv("GITHUB_APPLICATION_ID")
+	clusterName := os.Getenv("AWS_ECS_CLUSTER_NAME")
+	task := os.Getenv("AWS_ECS_CLUSTER_TASK")
+	webhookSecret := os.Getenv("WEBHOOK_SECRET")
+	assignPublicIP := os.Getenv("AWS_ECS_TASK_PUBLIC_IP")
+
+	publicIP, err := strconv.ParseBool(assignPublicIP)
+	if err != nil {
+		fmt.Println("Error parsing value for AWS_ECS_TASK_PUBLIC_IP")
+	}
+
+	eventType := request.Headers["X-GitHub-Event"]
+	signature := request.Headers["X-Hub-Signature-256"]
+
+	valid, err := validateGitHubSignature(request.Body, signature, []byte(webhookSecret))
+	if err != nil {
+		fmt.Printf("Error validating signature: %v\n", err)
+	}
+
+	if !valid {
+		fmt.Printf("Invalid signature\n")
+	} else {
+		fmt.Printf("Valid signature\n")
+	}
+
+	event, err := github.ParseWebHook(eventType, []byte(request.Body))
+	if err != nil {
+		fmt.Printf("Error parsing webhook: %s\n", err)
+	}
+
+	switch e := event.(type) {
+	case *github.InstallationRepositoriesEvent:
+		config := service.ECSConfig{
+			Cluster:  clusterName,
+			Task:     task,
+			PublicIP: publicIP,
+		}
+		svc := service.NewRenovateTaskService(config)
+		installationID := strconv.FormatInt(*e.Installation.ID, 10)
+
+		for _, repository := range e.RepositoriesAdded {
+			repo := fmt.Sprintf("%s/%s", repository.GetOwner().GetLogin(), repository.GetName())
+
+			taskConfig := service.RunTaskConfig{
+				ApplicationID:  applicationID,
+				InstallationID: installationID,
+				Repository:     repo,
+			}
+
+			_, err := svc.RunTask(taskConfig)
+			if err != nil {
+				fmt.Printf("Error running task: %s\n", err)
+			}
+		}
+	default:
+		fmt.Printf("Unhandled event type: %s\n", eventType)
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       "",
+		StatusCode: 200,
+	}, nil
+}
+
+func validateGitHubSignature(body string, signature string, secret []byte) (bool, error) {
+	if signature == "" {
+		return false, fmt.Errorf("missing X-Hub-Signature-256 header")
+	}
+
+	// Ensure the signature starts with "sha256="
+	if !strings.HasPrefix(signature, "sha256=") {
+		return false, fmt.Errorf("invalid X-Hub-Signature-256 header format")
+	}
+
+	// Extract the signature value (without the "sha256=" prefix)
+	expectedSignature := signature[7:]
+
+	// Compute the HMAC-SHA256 hash of the body using the secret
+	mac := hmac.New(sha256.New, secret)
+	mac.Write([]byte(body))
+	computedSignature := hex.EncodeToString(mac.Sum(nil))
+
+	// Compare the computed signature with the expected signature
+	if hmac.Equal([]byte(computedSignature), []byte(expectedSignature)) {
+		return true, nil
+	}
+
+	return false, fmt.Errorf("invalid signature")
+}
+
+func main() {
+	lambda.Start(handler)
+}
